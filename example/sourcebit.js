bbb8e7af6611d7b4ffe69b80053c1c217c1ced4f
Martin Makarsky <martinmakarsky@gmail.com>
2020-03-27 21:33:16 +0100

    Add projectId to test configuration to example project.

--- example/sourcebit.js
+++ example/sourcebit.js
@@ -3,7 +3,9 @@ module.exports = {
     {
       module: require("../index"),
       options: {
-        titleCase: true
+        titleCase: true,
+        cache: false,
+        kontentProjectId: "00676a8d-358c-0084-f2f2-33ed466c480a"
       }
     }
   ]
