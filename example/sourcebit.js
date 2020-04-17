5e3601208d746e76d8a86e5cfa24dc30ba83bcd3
Martin Makarský <martinm@kentico.com>
2020-04-17 14:06:53 +0200

    Normalize entry properly.

--- example/sourcebit.js
+++ example/sourcebit.js
@@ -5,7 +5,7 @@ module.exports = {
       options: {
         kontentProjectId: '00676a8d-358c-0084-f2f2-33ed466c480a',
         kontentLanguageCodenames: [
-          'en-US',
+          'us-US',
           'cs-CZ'
         ]
       }
@@ -26,73 +26,10 @@ module.exports = {
           return {
             content: {
               body: fields['content'],
-              frontmatter: { ...frontmatterFields, layout: 'article-layout' },
+              frontmatter: { ...frontmatterFields, layout: 'my-layout-name' },
             },
             format: 'frontmatter-md',
             path: '_article/' + createdAt.substring(0, 10) + '-' + utils.slugify(fields['title']) + '.md'
-          };
-        }
-        if (modelName === 'author' && projectId === '00676a8d-358c-0084-f2f2-33ed466c480a' && source === 'sourcebit-source-kontent') {
-          const { __metadata, ...fields } = entry;
-          return {
-            append: false,
-            content: fields,
-            format: 'json',
-            path: '_data/author.json'
-          };
-        }
-        if (modelName === 'category' && projectId === '00676a8d-358c-0084-f2f2-33ed466c480a' && source === 'sourcebit-source-kontent') {
-          const { __metadata, ...fields } = entry;
-          return {
-            append: false,
-            content: fields,
-            format: 'json',
-            path: '_data/category.json'
-          };
-        }
-        if (modelName === 'menu' && projectId === '00676a8d-358c-0084-f2f2-33ed466c480a' && source === 'sourcebit-source-kontent') {
-          const { __metadata, ...fields } = entry;
-          return {
-            append: false,
-            content: fields,
-            format: 'json',
-            path: '_data/menu.json'
-          };
-        }
-        if (modelName === 'menu_item' && projectId === '00676a8d-358c-0084-f2f2-33ed466c480a' && source === 'sourcebit-source-kontent') {
-          const { __metadata, ...fields } = entry;
-          return {
-            append: false,
-            content: fields,
-            format: 'json',
-            path: '_data/menu_item.json'
-          };
-        }
-        if (modelName === 'page' && projectId === '00676a8d-358c-0084-f2f2-33ed466c480a' && source === 'sourcebit-source-kontent') {
-          const { __metadata, ...fields } = entry;
-          return {
-            append: false,
-            content: fields,
-            format: 'json',
-            path: '_data/page.json'
-          };
-        }
-        if (modelName === 'site_metadata' && projectId === '00676a8d-358c-0084-f2f2-33ed466c480a' && source === 'sourcebit-source-kontent') {
-          const { __metadata, ...fields } = entry;
-          return {
-            append: false,
-            content: fields,
-            format: 'json',
-            path: '_data/site_metadata.json'
-          };
-        }
-        if (modelName === 'tag' && projectId === '00676a8d-358c-0084-f2f2-33ed466c480a' && source === 'sourcebit-source-kontent') {
-          const { __metadata, ...fields } = entry;
-          return {
-            append: false,
-            content: fields,
-            format: 'json',
-            path: '_data/tag.json'
           };
         }
         }
